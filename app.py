f0e2b793a26c827d057130f4df7fdc2dbeb022c3
ShiVmaur <mauryashivam74607@email.com>
2026-06-22 13:10:39 +0530

    Fixed DB connection

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ def clean_sql(query):
     return query.strip()
 
 # ── Engine ─────────────────────────────────────────────
-DB_URL = "postgresql+psycopg2://postgres.xrmpeerpgaxjghszaemt:OXi7Lh1ooaVNvYy5@aws-0-ap-northeast-1.pooler.supabase.com:5432/postgres"
-engine = create_engine(DB_URL, connect_args={"sslmode": "require"})
+DB_URL = st.secrets["DATABASE_URL"]
+engine = create_engine(DB_URL)
 
 # ── History table ──────────────────────────────────────
 with engine.connect() as conn:
